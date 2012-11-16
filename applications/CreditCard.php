b77b692d437aaaa12a4e77bec825d30571306673
James Crow <crow.jamesm@gmail.com>
2012-11-16 08:48:23 -0500

    Add luhn check function to CreditCard type.

--- applications/CreditCard.php
+++ applications/CreditCard.php
@@ -20,7 +20,10 @@ class CreditCard {
 		if (strlen($this->card_no) < 14 || !is_numeric($this->card_no)) {
 			throw new Exception("Card number ({$this->card_no})fails minumum checks!");
 		}
-		$this->card_type = $this->CreditCardType($this->card_no);
+		$card_type = $this->CreditCardType($this->card_no);
+		if ($card_type !== 'UNKNOWN' && luhn_check($this->card_no)) {
+			$this->card_type = $card_type;
+		}
 	}
 	
 	public function CreditCardType($CardNo) {
@@ -38,10 +41,6 @@ class CreditCard {
 		 */
 		
 		$CreditCardType = $this->card_type;
-		
-		//echo "Trying to determine card type for card number: {$CardNo}\n";
-		// test card number
-		// 3566007770017510 == 16 chars
 
 		//Check that the minimum length of the string isn't less
 		//than fourteen characters and -is- numeric
@@ -100,6 +99,40 @@ class CreditCard {
 
 		return $CreditCardType;
 	}
+	
+	/* Luhn algorithm number checker - (c) 2005-2008 shaman - www.planzero.org *
+	* This code has been released into the public domain, however please      *
+	* give credit to the original author where possible.                      */
+
+   function luhn_check($number) {
+
+	 // Strip any non-digits (useful for credit card numbers with spaces and hyphens)
+	 $number=preg_replace('/\D/', '', $number);
+
+	 // Set the string length and parity
+	 $number_length=strlen($number);
+	 $parity=$number_length % 2;
+
+	 // Loop through each digit and do the maths
+	 $total=0;
+	 for ($i=0; $i<$number_length; $i++) {
+	   $digit=$number[$i];
+	   // Multiply alternate digits by two
+	   if ($i % 2 == $parity) {
+		 $digit*=2;
+		 // If the sum is two digits, add them together (in effect)
+		 if ($digit > 9) {
+		   $digit-=9;
+		 }
+	   }
+	   // Total up the digits
+	   $total+=$digit;
+	 }
+
+	 // If the total mod 10 equals 0, the number is valid
+	 return ($total % 10 == 0) ? TRUE : FALSE;
+
+   }
 }
 
 ?>
