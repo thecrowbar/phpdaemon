f2db983cc66bdb3b5b61b1c835736a9cb2335181
James Crow <crow.jamesm@gmail.com>
2013-05-20 20:50:54 -0400

    Lots of changes to reprocess a log file with errors

--- applications/Vendor.stdin_log.php
+++ applications/Vendor.stdin_log.php
@@ -39,6 +39,7 @@
 	<div id="content">
 		<h2>Vendor Process stdin.log File</h2>
 		<?php
+		$sqli = new mysqli('127.0.0.1', 'test', 'test', 'double_drafts');
 		/**
 	 * Bytes that indicate the start of a message in the TCP data
 	 * @var String
@@ -65,6 +66,8 @@
 		
 		$pkt_array = array();
 		
+		$receipt_nums = array();
+		
 		// read our buffer from the job
 		$buffer = $this->job->getResult('read_buffer');
 		echo "We have ".strlen($buffer)." bytes of buffer to process!";
@@ -72,12 +75,19 @@
 		// open log to save packets into
 		$out_log = fopen('packets.log', 'w+');
 		$reject_log = fopen('packet_rejects.log', 'w+');
+		$duplicate_log = fopen('duplicate_in_log','w+');
+		//$duplicate_out_log = fopen('duplicate_out_log', 'w+');
 		
 		// loop over our buffer and split into individual packets
 		$startsize = strlen($pkt_start_bytes);
 		$endsize = strlen($pkt_end_bytes);
 
 		start:
+			// run any pending jobson the app object
+			//$this->app->job();
+//			if(count($receipt_nums) > 20000) {
+//				exit();
+//			}
 
 		// reset all variables
 		$pkt_start = 0;
@@ -101,7 +111,7 @@
 //			return;
 //		}
 		if (Daemon::$debug) {
-			Daemon::log('Found packet start at offset: ' . $pkt_start);
+			//Daemon::log('Found packet start at offset: ' . $pkt_start);
 		}
 		// good we found a packet, get our size
 		list (, $payload_size) = unpack('n', binarySubstr($buffer, $pkt_start + $startsize, $pkt_size_bytes));
@@ -185,8 +195,147 @@
 
 			
 			$pkt_count = count($pkt_array);
-			Daemon::log('We now have '.$pkt_count.' packets to process'."\n");
-			$this->app->processReceivedData($current_packet, $this->app->vendorconn, $this->app);
+			//Daemon::log('We now have '.$pkt_count.' packets to process');
+			if ($pkt_count % 100 === 0) {
+				Vendor::logger(Vendor::LOG_LEVEL_DEBUG, 'We now have '.$pkt_count.' packets to process');
+			}
+			//$this->app->processReceivedData($current_packet, $this->app->vendorconn, $this->app);
+			$iso = new ISO8583Trans();
+			
+//			// this is for the stdout.log data
+//			try{
+//				$iso->addTCPMessage($current_packet);				
+//				$time = $iso->getDataForBit(12, true); // HHMMSS
+//				$date = $iso->getDataForBit(13, true); // MMDD
+//				$submit_dt = '2013-'.substr($date,0,1).'-'.substr($date,-2).' '.substr($time,0,1).':'.substr($time,1,2).':'.substr($time,-2);
+//				$trans_id = $iso->getDataForBit(37);
+////				if (in_array($trans_id, $receipt_nums)) {
+////					fwrite($duplicate_out_log, $current_packet);
+////				} else {
+////					$receipt_nums[] = $trans_id;
+////				}
+//				$sql = "INSERT INTO trans (submit_dt, trans_id) VALUES('{$submit_dt}', '{$trans_id}')";
+//				Daemon::log('QUERY: '.$sql);
+//				$sqlR = $sqli->query($sql);
+//				if ($sqlR !== true){
+//					Daemon::log('ERROR Query failed:'.$sql);
+//				}
+//				//$this->appInstance->vendorconn->sendData($current_packet);
+//			}catch(Exception $e) {
+//				Daemon::log('Exception CAUGHT! $e:'.$e);
+//				Daemon::log('isoTCPMsg:'.bin2hex($current_packet));
+//			}
+			
+			// 2013-05-01 this is for the stdin.log file
+			try {
+				$iso->addTCPMessage($current_packet);
+				$trans_id = $iso->getDataForBit(37);
+				$response_code = $iso->getDataForBit(39, true);
+				try{
+					$auth_iden = $iso->getDataForBit(38, true);
+				}catch(Exception $e){
+					Daemon::log('Bit 38 data not found for transaction:'.$trans_id.', response_code:'.$response_code);
+					$auth_iden = '';
+				}
+				try{
+					$avs_resp = $iso->getDataForBit(44, true);
+				} catch(Exception $e) {
+					Daemon::log('Bit 44 data not found for transaction:'.$trans_id.', response_code:'.$response_code);
+					$avs_resp = '';
+				}
+				// get the id of this transaction
+				$receipt_number = $trans_id;
+				
+				// we now grab all records that match the given receipt_number and then 
+				// check each one
+				$sqlT = "SELECT id, receipt_number, response_code, auth_iden_response, avs_response FROM fd_draft_data WHERE receipt_number = {$trans_id}";
+				$sqlTR = $sqli->query($sqlT);
+				if ($sqlTR === false) {
+					echo "Query: {$sqlT} failed due to:".$sqli->error."<br />";
+					continue;
+				} else {
+//					$ta = $sqlTR->fetch_all();
+//					//$tr = $sqlTR->fetch_array();
+					while($tr = $sqlTR->fetch_assoc()){
+						if($tr['response_code'] === ''){
+							$trans_id = $tr['id'];
+							break;
+						}
+					}
+					$sqlTR->free();
+				}
+				Daemon::log('Updating trans with bit37: '.$receipt_number);
+				
+				// now get the rest of the transaction data from the real fd_draft_data table
+				$pri_acct_no_sql = '';
+				$user_name_sql = '';
+				$site_id_sql = '';
+				$frozen_sql = '';
+				$pkg_description_sql = '';
+				$uv_sql = '';
+				$draft_amount_sql = '';
+				$tan_tax_amount_sql = '';
+				$sqlF = "SELECT * FROM ultratan_drafts.fd_draft_data WHERE schedule_date = '2013-05-01' AND receipt_number = {$receipt_number}";
+				$sqlFR = $sqli->query($sqlF);
+				if ($sqlFR === false) {
+					Daemon::log('ERROR Query failed:'.$sql.', error:'.$sqli->error);
+				} else {
+					$tr = $sqlFR->fetch_assoc();
+					$pri_acct_no_sql = "pri_acct_no='{$tr['pri_acct_no']}', ";
+					$user_name_sql = "user_name='{$tr['user_name']}', ";
+					$site_id_sql = "site_id='{$tr['site_id']}', ";
+					$frozen_sql = "frozen='{$tr['frozen']}', ";
+					$pkg_description_sql = "pkg_description='{$tr['pkg_description']}', ";
+					$uv_sql = "uv='{$tr['uv']}', ";
+					$draft_amount_sql = "draft_amount='{$tr['draft_amount']}', ";
+					$tan_tax_amount_sql = "tan_tax_amount='{$tr['tan_tax_amount']}' ";
+					$sqlFR->free();
+				}
+					
+				// update our fd_draft_data with the new data
+				$sql = "UPDATE fd_draft_data SET 
+						response_code='{$response_code}', auth_iden_response='{$auth_iden}', avs_response='{$avs_resp}',
+						{$pri_acct_no_sql} {$user_name_sql} {$site_id_sql}
+						{$frozen_sql} {$pkg_description_sql} {$uv_sql}
+						{$draft_amount_sql} {$tan_tax_amount_sql}
+						WHERE id = {$trans_id}";
+				
+//				$sql = "INSERT INTO response (submit_dt, trans_id, response_code, auth_iden) VALUES('{$submit_dt}', '{$trans_id}', '{$response_code}', '{$auth_iden}')";
+				//Daemon::log('QUERY: '.$sql);
+				$sqlR = $sqli->query($sql);
+				if ($sqlR !== true){
+					Daemon::log('ERROR Query failed:'.$sql);
+				}
+			}catch(Exception $e) {
+				Daemon::log('Exception CAUGHT! $e:'.$e);
+				Daemon::log('ERROR HexTCP:'.bin2hex($current_packet));
+			}
+			
+			// 2013-05-02 This section is for creating new transaction entries
+			// from the duplicate ISO messages
+//			try{
+//				$iso->addTCPMessage($current_packet);
+//				//FIXME The $trans_table should not be hard-coded
+//				$queries = SQL::buildQueriesForTransInsert($iso, 'fd_draft_data');
+//				$sqlR = $sqli->query($queries[0]);
+//				if ($sqlR !== true){
+//					Daemon::log('ERROR Query failed:'.$queries[0].', error:'.$sqli->error);
+//				}
+//				// add our extra table entries
+//			}catch(Exception $e){
+//				Vendor::logger(Vendor::LOG_LEVEL_NOTICE, 'Exception re-creating ISO! $e:'.$e);
+//				Vendor::logger(Vendor::LOG_LEVEL_NOTICE, 'tcpData:'.bin2hex($current_packet));
+//			}
+			
+//			// 2013-05-02 This section for processing the duplciate in log
+//			try{
+//				$iso->addTCPMessage($current_packet);
+//			}catch(Exception $e) {
+//				Vendor::logger(Vendor::LOG_LEVEL_NOTICE, 'Exception re-creating ISO! $e:'.$e);
+//				Vendor::logger(Vendor::LOG_LEVEL_NOTICE, 'tcpData:'.bin2hex($current_packet));
+//			}
+			
+			
 		}
 		
 			
